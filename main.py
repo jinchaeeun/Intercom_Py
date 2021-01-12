879b571405bfee4a6de9981433da31ba427d2f65
HSJ <dk07564@gmail.com>
2021-01-13 02:54:11 +0900

    마이크로 소리 녹음

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import speech_recognition as sr
+import logging
 
-r = sr.Recognizer()
-harvard = sr.AudioFile('hello.wav')
-with harvard as source:
-    audio = r.record(source)
-print(r.recognize_google(audio, language='ko-KR'))
+logger = logging.Logger('catch_all')
+
+#pip install pyaudio(에러 시: pip install pipwin, pipwin install pyauidio 차례로 실행)
+
+def STT():
+    r = sr.Recognizer()
+
+    # print(sr.Microphone.list_microphone_names()) #마이크 목록
+
+    #마이크 목록에서 기기 선택
+    mic = sr.Microphone(device_index=1)
+
+    #마이크로부터 녹음
+    with mic as source:
+        audio = r.listen(source)
+
+    try:
+        return r.recognize_google(audio, language='ko-KR')
+
+    #마이크에 녹음되지 않았을 때
+    except Exception as ex:
+        logger.error(ex)
+        return "ERROR"
+
+print(STT())
